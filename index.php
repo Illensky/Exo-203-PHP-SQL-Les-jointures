077f9af956ddb3ca2abf79eeb2f9446ebc4abaf1
Illensky <alexis.laroche.02240@gmail.com>
2022-02-25 13:55:50 +0100

    Exercice done

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php
 
-$servername = 'localhost';
-$username = '';
-$password = '';
-$dbname = '';
+require __DIR__ . '/Classes/DBSingleton.php';
 
-?>
+$pdo = DBSingleton::PDO();
+
+$stm = $pdo->prepare("
+    SELECT eleve.*, eleve_information.*
+    FROM eleve
+    INNER JOIN eleve_information ON eleve.information_fk = eleve_information.id
+");
+
+if ($stm->execute()) {
+    echo "<pre>";
+    print_r($stm->fetchAll());
+    echo "</pre>";
+}
+
+$stm = $pdo->prepare("
+    SELECT eleve_competence.niveau, competence.titre, eleve.prenom, eleve.nom
+    FROM eleve_competence
+    INNER JOIN competence ON eleve_competence.competence_fk = competence.id
+    INNER JOIN eleve ON eleve_competence.eleve_fk = eleve.id
+");
+
+if ($stm->execute()) {
+    echo "<pre>";
+    print_r($stm->fetchAll());
+    echo "</pre>";
+}
